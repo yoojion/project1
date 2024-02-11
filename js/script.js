0bd068380ff2ad7c24f8be3da3241b806bbae7c5
유지원 <yoovely@yujiwon-ui-MacBookPro.local>
2024-02-12 01:28:54 +0900

    upbtn추가

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // vanilla js
-const pop = document.querySelector('.popup')
-const btn = document.querySelector('.popup>span')
-btn.onclick=function () {
-    pop.style.display = 'none';
+const pop = document.querySelector(".popup");
+const btn = document.querySelector(".popup>span");
+btn.onclick = function () {
+  pop.style.display = "none";
 };
+
+const up_btn = document.querySelector("up_btn");
