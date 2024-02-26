b39099f37078cd03e3b80dceffdb54d21418a3c6
yoojion <wldnjsdl886@naver.com>
2024-02-26 12:18:51 +0900

    시계추가

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@ btn.onclick = function () {
 };
 
 const up_btn = document.querySelector("up_btn");
+
+//시계
+function time() {
+  let day = new Date(); //인스턴스객체(=함수)
+  //toLocaleTimeString() : 현지시간 표시
+  let now = day.toLocaleTimeString();
+  console.log(now);
+  document.querySelector("#time").innerHTML = now;
+}
+setInterval(time, 1000);
